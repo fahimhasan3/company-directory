88276e05079b64044e6852b6435c7130664ad3bd
fahimhasan3 <60883810+fahimhasan3@users.noreply.github.com>
2020-09-29 17:57:02 +0100

    production settings

--- server/php/config.php
+++ server/php/config.php
@@ -1,10 +1,10 @@
 <?php
 
-	$cd_host = "127.0.0.1";
+	$cd_host = "f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
 	$cd_port = 3306;
 	$cd_socket = "";
-	$cd_user = "root";
-	$cd_password = "root";
-	$cd_dbname = "companydirectory";
+	$cd_user = "csbknshnz70ae98f";
+	$cd_password = "f611c0ptln6eqy8e";
+	$cd_dbname = "uiqgn79y9nhwo5v4";
 
 ?>
